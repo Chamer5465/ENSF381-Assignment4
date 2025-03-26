76218891d6e256bfcf62e9a16b4603d26d3157b0
lreed03 <lreed0383@gmail.com>
2025-03-26 17:58:21 -0600

    Started work on the course page

--- my-lms-app/src/CourseCatalog.js
+++ my-lms-app/src/CourseCatalog.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import CourseItem from './CourseItem';
+import Courses from './data/courses';
 
 function CourseCatalog() {
+    const course = Courses[1];
+    var name = course.name;
+    var instructor = course.instructor;
+    var description = course.description;
     return (
         <div>
-
+            <CourseItem name={name} instructor={instructor} description={description}/>
         </div>
     );
 }
--- my-lms-app/src/CourseItem.js
+++ my-lms-app/src/CourseItem.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import {React, useState} from 'react';
+import courselogo from './images/course1.jpg';
+import Courses from './data/courses';
 
 function CourseItem(props) {
-    return (
-        <div>
+    const [showDescription, setShowDescription] = useState(false);
 
+    function hover(){
+        setShowDescription(true);
+    }
+
+    function outHover(){
+        setShowDescription(false);
+    }
+
+    return (
+        <div onMouseEnter={hover} onMouseLeave={outHover}>
+            <img src={courselogo} alt='Course Image'></img>
+            <p>Course Name: {props.name}</p>
+            <p>Instructor: {props.instructor}</p>
+            {showDescription && <p>Description: {props.description}</p>}
+            <button>Enroll Now</button>
         </div>
     );
 }

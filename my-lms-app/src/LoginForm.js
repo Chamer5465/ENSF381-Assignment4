13ceac76ff0a46e8abd5d1047c23239e2ddcd3f7
lreed03 <lreed0383@gmail.com>
2025-03-26 16:49:56 -0600

    Made initial changes to login form

--- my-lms-app/src/LoginForm.js
+++ my-lms-app/src/LoginForm.js
@@ -3,7 +3,14 @@ import React from 'react';
 function LoginForm() {
     return (
         <div>
-
+            <form>
+                <label for='username'>Username:</label>
+                <input type='text' id='username' required></input>
+                <label for='password'>Password:</label>
+                <input type='text' id='password' required></input>
+                <button type='submit'>Submit</button>
+                <a href=''>Forgot Password:</a>
+            </form>
         </div>
     );
 }
